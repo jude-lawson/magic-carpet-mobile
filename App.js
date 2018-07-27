695329a0afc4fde8d7c61b45a0fed9dc398035a4
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-27 11:29:48 -0600

    Pulls Gradient component out to its own class

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { LinearGradient } from 'expo';
 
 import LoginPage from './src/components/LoginPage'
+import Gradient from './src/components/Gradient'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <LinearGradient 
-          colors={['#03063b', '#7998fe']} 
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            height: 1000
-          }}
-        />
-          <LoginPage />
-        {/* </LinearGradient> */}
+        <Gradient />
+        <LoginPage />
       </View>
     );
   }
