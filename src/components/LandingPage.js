fb33044e6dc39b0456663bbf34b8bd311d0156ea
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-29 14:22:24 -0600

    Currently hard codes in universal link, but works for calling Lyft.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text } from 'react-native';
+import { WebBrowser } from 'expo';
 
 import SettingsIcon from './SettingsIcon';
 import MagicCarpetButton from './MagicCarpetButton';
@@ -8,12 +8,14 @@ import EstimatePage from './EstimatePage';
 import HomeButton from './HomeButton';
 import SettingsPage from './SettingsPage';
 import UserAvatar from './UserAvatar';
+import { lyft_client_id } from '../../config.js'
 
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       content: 'This is the current body',
+      loggedIn: true,
       rideCalled: false,
       openSettings: false
     }
@@ -21,32 +23,38 @@ export default class LandingPage extends Component {
     this.createAdventure = this.createAdventure.bind(this)
     this.handleHomeClick = this.handleHomeClick.bind(this)
     this.renderSettingsPage = this.renderSettingsPage.bind(this)
+    this.openLyft = this.openLyft.bind(this)
   }
 
   createAdventure() {
-    fetch('http://localhost:3000/api/v1/adventures', {
-      method: 'POST',
-      body: JSON.stringify({
-        preferences: {
-        "open_now": true,
-        "radius": 1000,
-        "latitude": 39.7293,
-        "longitude": -104.9844,
-        "price": "1,2,3",
-        "term": "restaurants"
-        }
-      })
-    })
-     .then((response) => response.json())
-     .then((parsedResponse) => {
-       this.setState(() => ({
-         rideCalled: true,
-         content: parsedResponse.destination
-       }))
-     })
-     .catch((error) => {
-       console.error(error);
-     })
+    // fetch('http://localhost:3000/api/v1/adventures', {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     preferences: {
+    //     "open_now": true,
+    //     "radius": 1000,
+    //     "latitude": 39.7293,
+    //     "longitude": -104.9844,
+    //     "price": "1,2,3",
+    //     "term": "restaurants"
+    //     }
+    //   })
+    // })
+    //  .then((response) => response.json())
+    //  .then((parsedResponse) => {
+    //    this.setState(() => ({
+    //      rideCalled: true,
+    //      content: parsedResponse.destination
+    //    }))
+    //  })
+    //  .catch((error) => {
+    //    console.error(error);
+    //  })
+    return
+  }
+
+  openLyft() {
+    WebBrowser.openBrowserAsync(`https://lyft.com/ride?id=lyft&pickup[latitude]=39.9721648&pickup[longitude]=-105.08742&partner=${lyft_client_id}&destination[latitude]=39.9877087&destination[longitude]=-105.0858611`)
   }
 
   handleHomeClick() {
@@ -63,19 +71,30 @@ export default class LandingPage extends Component {
 
   render() {
     let pageContent;
-    if (!this.state.rideCalled && !this.state.openSettings) {
+    // if (!this.state.rideCalled && !this.state.openSettings) {
+    //   pageContent = (
+    //     <React.Fragment>
+    //       {/* <HomeButton handleHomeClick={this.handleHomeClick} />
+    //       <UserAvatar />
+    //       <SettingsIcon renderSettings={this.renderSettingsPage} />
+    //       <MagicCarpetButton clickEvent={this.createAdventure} /> */}
+    //       <Text>Nope.</Text>
+    //     </React.Fragment>
+    //   );
+    // } else 
+    if (this.state.rideCalled) {
+      pageContent = <EstimatePage price={this.state.content.price} data={this.state.content} />
+    } else if (this.state.openSettings) {
+      pageContent = <SettingsPage />
+    } else if (this.state.loggedIn) {
       pageContent = (
         <React.Fragment>
           <HomeButton handleHomeClick={this.handleHomeClick} />
           <UserAvatar />
           <SettingsIcon renderSettings={this.renderSettingsPage} />
-          <MagicCarpetButton clickEvent={this.createAdventure} />
+          <MagicCarpetButton clickEvent={this.openLyft} />
         </React.Fragment>
       );
-    } else if (this.state.rideCalled) {
-      pageContent = <EstimatePage price={this.state.content.price} data={this.state.content} />
-    } else if (this.state.openSettings) {
-      pageContent = <SettingsPage />
     }
 
     return (
